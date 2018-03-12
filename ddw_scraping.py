0c395287d31cd7ab71dfae0e83cba8f2e0a0d6de
Craig Corcoran <craig@newknowledge.io>
2018-03-11 23:21:52 -0500

    check for content before printing during error.

--- ddw_scraping.py
+++ ddw_scraping.py
@@ -114,7 +114,8 @@ def process_bucket_object(obj_key, base_dir, base_url, req_params, bucket_name, 
 
         except Exception as e:
             print('error with metadata in:', metadata_fname)
-            print('response:', content)
+            if content:
+                print('response:', content)
             print('error:', e)
             raise e
 
